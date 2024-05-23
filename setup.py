9ea079ba7bb33fe46a7f533a58542049d5121e47
Hari prasad <hariprasad9693@gmail.com>
2024-05-23 11:12:02 -0700

    intia commit with cookicutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project helps to make decision about those customers who  are likely to churn',
+    author='Hari Prasad',
+    license='',
+)
